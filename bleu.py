40edd97aa8ab19330f6a9ae28dba56d44e1a0ac9
David Chiang <dchiang@nd.edu>
2023-09-18 16:23:27 -0400

    update bleu.py to python3 and simplify api

--- bleu.py
+++ bleu.py
@@ -1,9 +1,6 @@
 """Simple implementation of single-reference, case-sensitive BLEU
 without tokenization."""
 
-from __future__ import division
-from six.moves import range, zip
-from six import itervalues
 import collections
 import math
 
@@ -15,7 +12,7 @@ def ngrams(seg, n):
 
 def card(c):
     """Cardinality of a multiset."""
-    return sum(itervalues(c))
+    return sum(c.values())
 
 def zero():
     return collections.Counter()
@@ -31,34 +28,36 @@ def count(t, r, n=4):
     stats['reflen'] += len(r)
     return stats
 
-def score(stats, n=4):
+def score(test_sents, ref_sents, n=4):
     """Compute BLEU score.
 
-    :param stats: Statistics collected using bleu.count
-    :type stats: dict"""
+    Arguments:
+    - test_sents: test sentences (list of list of strs)
+    - ref_sents: reference sentences (list of list of strs)"""
 
+    c = zero()
+    for t, g in zip(test_sents, ref_sents):
+        c += count(t, g, n=args.n)
+        
     b = 1.
     for i in range(1, n+1):
-        b *= stats['match',i]/stats['guess',i] if stats['guess',i] > 0 else 0
+        b *= c['match',i]/c['guess',i] if c['guess',i] > 0 else 0
     b **= 0.25
-    if stats['guess',1] < stats['reflen']: 
-        b *= math.exp(1-stats['reflen']/stats['guess',1])
+    if c['guess',1] < c['reflen']: 
+        b *= math.exp(1-c['reflen']/c['guess',1])
     return b
 
 if __name__ == "__main__":
     import argparse
 
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('test', metavar='predict', help='predicted translations')
-    argparser.add_argument('gold', metavar='true', help='true translations')
+    argparser.add_argument('test_file', metavar='predict', help='predicted translations')
+    argparser.add_argument('ref_file', metavar='true', help='true translations')
     argparser.add_argument('-n', help='maximum n-gram size to score', default=4, type=int)
     args = argparser.parse_args()
 
-    test = [line.split() for line in open(args.test)]
-    gold = [line.split() for line in open(args.gold)]
+    test_sents = [line.split() for line in open(args.test_file)]
+    ref_sents = [line.split() for line in open(args.ref_file)]
 
-    c = zero()
-    for t, g in zip(test, gold):
-        c += count(t, g, n=args.n)
-    print("BLEU:", score(c, n=args.n))
+    print("BLEU:", score(test_sents, ref_sents, n=args.n))
     
